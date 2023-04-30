589cd74e9ee2c4b8889d2fb9cd22d154af3377e0
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-04-30 14:06:33 +0530

    feat: mui && tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <>
-      <h1>Recruitment 2023</h1>
-    </>
-  );
+		<>
+			<h1 className='text-3xl font-bold underline'>Recruitment 2023</h1>
+		</>
+	)
 }
 
 export default App;
