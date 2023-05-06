c634e196f07e956c4a13da6b5bee40071bc13fe0
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-05-06 17:02:45 +0530

    feat: security

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app'
 import { getAnalytics, isSupported } from 'firebase/analytics'
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyCjSGyC0GEupCH7h6-Y2Bjz3Isq-gRdj3E',
-	authDomain: 'recruitment-23.firebaseapp.com',
-	projectId: 'recruitment-23',
-	storageBucket: 'recruitment-23.appspot.com',
-	messagingSenderId: '288600865743',
-	appId: '1:288600865743:web:77733cff4ce0a4d1c030ba',
-	measurementId: 'G-2GYZE0PP8F',
-}
+	apiKey: REACT_APP_APIKEY,
+	authDomain: REACT_APP_AUTHDOMAIN,
+	projectId: REACT_APP_PROJECTID,
+	storageBucket: REACT_APP_STORAGEBUCKET,
+	messagingSenderId: REACT_APP_MESSAGINGSENDERID,
+	appId: REACT_APP_APPID,
+	measurementId: REACT_APP_MEASUREMENTID,
+};
 
 const app = initializeApp(firebaseConfig)
 const analytics = isSupported().then((yes) => (yes ? getAnalytics(app) : null))
