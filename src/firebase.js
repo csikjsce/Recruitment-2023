81f6f3cddd723984d3f80f8997ca2e7f80bf6e7a
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-05-06 18:23:18 +0530

    removed: .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
-import { initializeApp } from 'firebase/app'
-import { getAnalytics, isSupported } from 'firebase/analytics'
+import { initializeApp } from 'firebase/app';
+import { getAnalytics, isSupported } from 'firebase/analytics';
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_APIKEY,
-	authDomain: process.env.REACT_APP_AUTHDOMAIN,
-	projectId: process.env.REACT_APP_PROJECTID,
-	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-	appId: process.env.REACT_APP_APPID,
-	measurementId: process.env.REACT_APP_MEASUREMENTID,
+	apiKey: 'AIzaSyCjSGyC0GEupCH7h6-Y2Bjz3Isq-gRdj3E',
+	authDomain: 'recruitment-23.firebaseapp.com',
+	projectId: 'recruitment-23',
+	storageBucket: 'recruitment-23.appspot.com',
+	messagingSenderId: '288600865743',
+	appId: '1:288600865743:web:77733cff4ce0a4d1c030ba',
+	measurementId: 'G-2GYZE0PP8F',
 };
 
-const app = initializeApp(firebaseConfig)
-const analytics = isSupported().then((yes) => (yes ? getAnalytics(app) : null))
+const app = initializeApp(firebaseConfig);
+const analytics = isSupported().then((yes) => (yes ? getAnalytics(app) : null));
 
-export { app, analytics }
+export { app, analytics };
