c5a3525f73e521748c6a04ceb9c7571ae7cefad7
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-05-06 17:09:46 +0530

    fix: .env bug

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getAnalytics, isSupported } from 'firebase/analytics'
 
 const firebaseConfig = {
-	apiKey: REACT_APP_APIKEY,
-	authDomain: REACT_APP_AUTHDOMAIN,
-	projectId: REACT_APP_PROJECTID,
-	storageBucket: REACT_APP_STORAGEBUCKET,
-	messagingSenderId: REACT_APP_MESSAGINGSENDERID,
-	appId: REACT_APP_APPID,
-	measurementId: REACT_APP_MEASUREMENTID,
+	apiKey: process.env.REACT_APP_APIKEY,
+	authDomain: process.env.REACT_APP_AUTHDOMAIN,
+	projectId: process.env.REACT_APP_PROJECTID,
+	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+	appId: process.env.REACT_APP_APPID,
+	measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig)
